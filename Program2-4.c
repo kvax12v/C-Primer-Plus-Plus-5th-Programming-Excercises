736b6ba86b03f4afab7d97e7580464a8500a2407
Hamed Ghafarshad <hg.truth@gmail.com>
2017-03-17 11:41:35 -0700

    CH2-4 Uploaded

---
+++ Program2-4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int jolly_good(void);
+int nobody_deny(void);
+int main(void){
+
+	jolly_good();
+	jolly_good();
+	jolly_good();
+	nobody_deny();
+
+
+}
+
+int jolly_good(void){
+
+	printf("For he's a jolly good fellow!\n");
+}
+
+int nobody_deny(void){
+	
+	printf("Which nobody can deny!\n");
+
+}
