5ca90965a427605042bcd3e264e1ee4bb248e74a
Hamed Ghafarshad <hg.truth@gmail.com>
2017-03-17 11:47:59 -0700

    CH2-5 Uploaded

---
+++ Program2-5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(void){
+	int toes = 10;
+	int toes_twice = toes * 2;
+	int toes_squared = toes * toes;
+	
+	printf("Number of toes : %d \nNumber of toes doubled : %d \nNumber of toes squared : %d \n", toes, toes_twice, toes_squared);
+
+	return 0;
+}
